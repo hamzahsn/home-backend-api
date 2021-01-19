8a0766b7b95a5e02ea310e4f240b913089e0cfb4
hamzahsn <amdouni.hamza@hotmail.fr>
2021-01-19 10:29:09 +0100

    Creating generic wrapper for routes

---
+++ src/utils/wrappers.ts
@@ -0,0 +1,16 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+type Handler = (req: FastifyRequest, res: FastifyReply, error?: Error) => Promise<void> | void
+
+type Route = {
+    url: string
+    method: string
+    handler: Handler | Handler[]
+}
+
+export const applyRoutes = async (routes: Route[], router: unknown) => {
+    for (const route of routes) {
+        const { method, url, handler } = route
+        ;(router as any)[method](url, handler)
+    }
+}
